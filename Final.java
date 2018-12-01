79400aadf963fa4f22f38a072e5bcd1989be09fa
kpatterson01 <kpatterson01@users.noreply.github.com>
2018-12-01 15:30:51 -0500

    Reader can now answer Questions from file

--- Final.java
+++ Final.java
@@ -4,10 +4,11 @@ import java.io.IOException;
 import java.io.File;
 import java.util.Arrays; 
 import java.io.*; 
+import java.util.ArrayList; 
 
 public class Final {
   public static void main (String [] args) throws IOException { 
-        Scanner in = new Scanner(System.in); 
+        
         /*String[] q1Choices = {"1.Saint Nick and Kris Kringle", "2.Santa Claus"}; 
         String[] q2Choices = {"1.Sandy", "2.Cupid"}; 
 
@@ -15,42 +16,49 @@ public class Final {
         Question q2 = new Question("One of Santa's reindeer shares a name with a famous symbol of Valentine's Day. Which Reindeer is that?", 1, q2Choices); 
         Question[] questions = new Question[]{q1, q2}; */
         
-        //Stores File (325) 
-        File file = new File("testFile.txt"); 
-        Question readQuestion = new Question(file); 
-                 
-        //open files
-        Scanner inputFile = new Scanner(file);
-        
+        Scanner in = new Scanner(System.in); 
+ 
+        ArrayList<Question> read = readQuestions("testFile.txt"); //readQuestions from text file 
                 
-        while(inputFile.hasNext()) {
-            //stores question and then prints 
-           String question = inputFile.nextLine(); 
-           System.out.println(question); 
-           //int userInput = in.nextInt(); 
-        }
-
-        
+        for (Question q : read) {
+            System.out.println(q.getQuestion() + "\n" + Arrays.toString(q.getChoices()).substring(1, q.length()-1));
+            int userInput = in.nextInt(); 
+   
+            if (isCorrectAnswer(q, userInput)) {
+               System.out.println("Hooray! You have entered the correct answer");  
+            } else {
+               System.out.println("You have entered the wrong answer"); 
+            } 
+         } 
                 
-        inputFile.close(); //close file
         
-                
                
   } //end of main method
   
-  public static String readQuestion(Question q) { 
-       for (Question q : questions) {
-         System.out.println(q.getQuestion() + "\n" + Arrays.toString(q.getChoices()));
-         int userInput = in.nextInt(); 
+  
+  public static ArrayList<Question> readQuestions (String fileName) throws IOException {
+    ArrayList<Question> q = new ArrayList<Question>(); 
+    
+    File file = new File(fileName); //Stores File    
+    Scanner inputFile = new Scanner(file); //open files
 
-         if (isCorrectAnswer(q, userInput)) {
-            System.out.println("Hooray! You have entered the correct answer");  
-         } else {
-            System.out.println("You have entered the wrong answer"); 
-            
-         } 
-       }   
-  }
+    while(inputFile.hasNext()) {
+      //prints question from file 
+      String line = inputFile.nextLine(); //reads question 
+      int correctIndex = inputFile.nextInt(); 
+      int numOptions = inputFile.nextInt(); 
+      inputFile.nextLine(); // clears buffer
+      String[] options = new String[numOptions]; 
+      for (int i = 0; i < options.length; i++) {
+         String reader = inputFile.nextLine(); 
+         options[i] = reader; 
+      }
+      Question question = new Question(line, correctIndex,options); 
+      q.add(question); //appends questions to arraylist    
+     }
+     inputFile.close(); //close file
+     return q; 
+  } //end of readQuestion method 
      
   public static boolean isCorrectAnswer (Question q, int userInput) {
      if ( userInput == (q.getAnswer() + 1)) {
--- Question.java
+++ Question.java
@@ -3,19 +3,13 @@ import java.io.IOException;
 import java.io.File;
 import java.util.Arrays; 
 import java.io.*; 
-//Class for Questions 
 
+//Class for Questions 
 public class Question {
-  public String question; 
-  public int posRightAnswer; 
-  public String[] choices; 
-  public File file; 
-  
-  public Question(File file) {
-  
-  
-  } 
-  
+  private String question; 
+  private int posRightAnswer; 
+  private String[] choices; 
+    
   public Question(String question, int posRightAnswer, String[] choices) { //constructor
     this.question = question; 
     this.posRightAnswer = posRightAnswer; 
@@ -33,6 +27,6 @@ public class Question {
   public int getAnswer() { //return true of the pos of right answer is correct 
    return posRightAnswer; 
   }
-  
+    
 } //end of class 
 
